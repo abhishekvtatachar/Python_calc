04778ac40be57008945f4c70a8a9b41026da34fc
abhishekvtatachar <134733858+abhishekvtatachar@users.noreply.github.com>
2023-09-16 16:04:25 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_result = add(num1, num2)
+difference_result = subtract(num1, num2)
+product_result = multiply(num1, num2)
+quotient_result = divide(num1, num2)
+
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
